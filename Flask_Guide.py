7aa12bd6c4bfcc8f8fa379249b17082e30f9d475
anna <anna2617@cmis.ac.th>
2019-08-21 11:57:00 +0700

    template installed

--- Flask_Guide.py
+++ Flask_Guide.py
@@ -1,20 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template 
 
-app = Flask(__name__)
+app = Flask(__name__) # variable 
 
 @app.route('/') # route() used to bind URL to a function 
-
+@app.route('/home') 
 # @app.route('/<name>')
 # variables can be added to pass the parameter, in thsi case, it will be <name>
 # ex: %s ' %'variable name' 
-
-
-def hello_world():
-   return "Hello World"
+def home():
+   return  render_template('home.html')#html source code 
 
 if __name__ == '__main__':
-   app.debug = True
-   app.run(host="0.0.0.0", port = 5000, debug = True) # runs the app on particular development server 
+   app.run(debug = True) # runs the app on particular development server 
   # app.run (host, ports, options)
 
 # Host: Hostname to listen on. Defaults to 127.0.0.1 (localhost). Set to ‘0.0.0.0’ to have server available externally
@@ -24,4 +21,8 @@ if __name__ == '__main__':
 # Debug:Defaults to false. If set to true, provides a debug information
 
 # Options:To be forwarded to underlying Werkzeug server.
+
+@app.route('/about')
+def about():
+    return "About page"
 
