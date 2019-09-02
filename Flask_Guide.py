194d37a929793dd672e551bf6b021350ffaec067
anna <anna2617-cmis>
2019-09-02 11:57:25 +0700

    js

--- Flask_Guide.py
+++ Flask_Guide.py
@@ -30,6 +30,11 @@ def login():
         user = request.args.get('nm')
         return redirect(url_for('hello_guest', guest = user))
 
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('result.html', result = dict)
+
 if __name__ == '__main__':
    app.run(debug = True) # runs the app on particular development server 
   # app.run (host, ports, options)
